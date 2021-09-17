65d4ed4b5e3f417d9f8785df853e46e556fb8f33
Lahiry <raphaellcmb@al.insper.edu.br>
2021-09-17 17:06:32 -0300

    class datagrama

--- protocolo_ponto_a_ponto/datagrama.py
+++ protocolo_ponto_a_ponto/datagrama.py
@@ -1,10 +1,13 @@
 class Datagrama:
 
-    def __init__(self, head, payload, eop):
-        self.head = head
-        self.payload = payload
-        self.eop = eop
+    def __init__(self):
+        self.head = b''
+        self.payload = b''
+        self.eop = b'\xFF\xAA\xFF\xAA'
 
-    def send_datagrama(self):
+    def create_head(h0, h1=b'x\01', h2=b'x\02', h3, h4, h5, h6, h7, h8=b'x\00', h9=b'x\00'):
+        self.head = h0 + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9
+
+    def format_datagrama(self):
         datagrama = self.head + self.payload + self.eop
         return datagrama
